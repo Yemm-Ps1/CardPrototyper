d78059ebd848a8982757202d779f09eb31a4ae9b
Alexander Yemm <alexyemm@gmail.com>
2021-11-22 21:51:25 +0000

    Compiled into exe using PS2EXE

--- New-Deck.psm1
+++ New-Deck.psm1
@@ -5,34 +5,13 @@
  .Description
   Creates a 
 
- .Parameter Start
-  The first month to display.
-
- .Parameter End
-  The last month to display.
-
- .Parameter FirstDayOfWeek
-  The day of the month on which the week begins.
-
- .Parameter HighlightDay
-  Specific days (numbered) to highlight. Used for date ranges like (25..31).
-  Date ranges are specified by the Windows PowerShell range syntax. These dates are
-  enclosed in square brackets.
-
- .Parameter HighlightDate
-  Specific days (named) to highlight. These dates are surrounded by asterisks.
+ .Parameter Deck
+  Specify the Deck (CSV File) you want to create. 
 
  .Example
-   # Show a default display of this month.
-   Show-Calendar
+   # Create the default deck in the input folder
+   New-Deck default
 
- .Example
-   # Display a date range.
-   Show-Calendar -Start "March, 2010" -End "May, 2010"
-
- .Example
-   # Highlight a range of days.
-   Show-Calendar -HighlightDay (1..10 + 22) -HighlightDate "December 25, 2008"
 #>
 function New-Deck {
     [CmdletBinding()]
@@ -41,9 +20,10 @@ function New-Deck {
     DynamicParam{
         $ParamAttrib = New-Object System.Management.Automation.ParameterAttribute
         $ParamAttrib.Position = 0
+        $ParamAttrib.HelpMessage = "Test"
         $AttribColl = New-Object System.Collections.ObjectModel.Collection[System.Attribute]
         $AttribColl.Add($ParamAttrib)
-        $AttribColl.Add((New-Object System.Management.Automation.ValidateSetAttribute(Get-ChildItem "$PSScriptRoot\input").BaseName))
+        $AttribColl.Add((New-Object System.Management.Automation.ValidateSetAttribute((Get-ChildItem "$PSScriptRoot\input").BaseName)))
         $RuntimeParam = New-Object System.Management.Automation.RuntimeDefinedParameter('Deck', [string], $AttribColl)
         $RuntimeParamDic = New-Object System.Management.Automation.RuntimeDefinedParameterDictionary
         $RuntimeParamDic.Add('Deck', $RuntimeParam)
